644c5e90003329d1a56845c656536306df6cb9ef
Mohd. Afnan <afnanmalik2712@gmail.com>
2022-12-21 19:35:34 +0530

    feat: Add new sources and kind for nvim-cmp

---
+++ lua/afnan/lsp/cmp-sources.lua
@@ -0,0 +1,45 @@
+---@diagnostic disable: unused-local
+local sources = {
+	{ name = "luasnip", max_item_count = 2 },
+	{ name = "nvim_lsp", max_item_count = 4 },
+	--[[ { name = "nvim_lsp_signature_help" }, ]]
+	{ name = "buffer", max_item_count = 2 },
+	{ name = "path" },
+	--[[ { name = "devicons", priority = 500 }, ]]
+}
+
+if vim.o.ft == "lua" then
+	table.insert(sources, { name = "nvim_lua" })
+end
+
+if vim.o.ft == "zsh" then
+	table.insert(sources, { name = "zsh" })
+	require("cmp_zsh").setup({
+		zshrc = true,
+		filetypes = { "zsh" },
+	})
+end
+
+--- Reference: https://github.com/hrsh7th/nvim-cmp/discussions/702
+--- nvim-web-devicons source for nvim-cmp
+--[[ function _G.DeviconsCompletion() ]]
+--[[ 	local cmp = require("cmp") ]]
+--[[ 	local devicons = require("nvim-web-devicons") ]]
+--[[ 	cmp.register_source("devicons", { ]]
+--[[ 		complete = function(self, params, callback) ]]
+--[[ 			local items = {} ]]
+--[[ 			for _, icon in pairs(devicons.get_icons()) do ]]
+--[[ 				table.insert(items, { ]]
+--[[ 					label = icon.icon .. "  " .. icon.name, ]]
+--[[ 					insertText = icon.icon, ]]
+--[[ 					filterText = icon.name, ]]
+--[[ 				}) ]]
+--[[ 			end ]]
+--[[ 			callback({ items = items }) ]]
+--[[ 		end, ]]
+--[[ 	}) ]]
+--[[ end ]]
+--[[]]
+--[[ vim.api.nvim_add_user_command("CmpDevicons", _G.DeviconsCompletion, {}) ]]
+--[[]]
+return sources
--- lua/afnan/lsp/cmp.lua
+++ lua/afnan/lsp/cmp.lua
@@ -1,28 +1,74 @@
+---@diagnostic disable: unused-local
 local cmp = require'cmp'
-
+local luasnip = require('luasnip')
+local kind = require('afnan.lsp.utils').kind()
+local border = require("afnan.lsp.utils").borders()
+local sources = require('afnan.lsp.cmp-sources')
+   
   cmp.setup({
     snippet = {
       expand = function(args)
-         require('luasnip').lsp_expand(args.body)
+         luasnip.lsp_expand(args.body)
       end,
     },
     window = {
-      -- completion = cmp.config.window.bordered(),
-      -- documentation = cmp.config.window.bordered(),
+
+    documentation = {
+       border = border
+         },
     },
     mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-e>'] = cmp.mapping.abort(),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+      ["<cr>"] = cmp.mapping.confirm({ select = true }),
+		["<C-e>"] = cmp.mapping.close(),
+      ["<C-a>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
     }),
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'luasnip' },
-    }, {
-      { name = 'buffer' },
-    })
+    experimental = { ghost_text = true },
+   formatting = {
+         fields = { "kind", "abbr" },
+         format = function(entry, vim_item)
+            vim_item.abbr = vim_item.abbr:sub(1, 30)
+            vim_item.kind = kind[vim_item.kind]
+            vim_item.dup = { buffer = 0, path = 0, nvim_lsp = 0, nvim_lua = 0 }
+            return vim_item
+         end,
+      },
+    sources = sources,
+   sorting = {
+         comparators = {
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            function(entry1, entry2)
+               local _, entry1_under = entry1.completion_item.label:find("^_+")
+               local _, entry2_under = entry2.completion_item.label:find("^_+")
+               entry1_under = entry1_under or 0
+               entry2_under = entry2_under or 0
+               if entry1_under > entry2_under then
+                  return false
+               elseif entry1_under < entry2_under then
+                  return true
+               end
+            end,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+         },
+      },
   })
 
   cmp.setup.filetype('gitcommit', {
@@ -49,3 +95,6 @@ local cmp = require'cmp'
   })
   })
 
+-- completion menu settings
+vim.opt.pumheight = 8
+vim.o.pumblend = 20
