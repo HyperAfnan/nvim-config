2f3033ee5748e50ee6abd65d84befecbe427ecbd
Mohd. Afnan <afnanmalik2712@gmail.com>
2022-12-28 22:01:38 +0530

    feat(ui): Add config for nvim-scrollbar

--- lua/afnan/plugins.lua
+++ lua/afnan/plugins.lua
@@ -57,8 +57,20 @@ require("lazy").setup({
 	{
 		"petertriho/nvim-scrollbar",
 		config = function()
+			local colors = require("tokyonight.colors").setup()
 			require("scrollbar").setup({
-				handler = { gitsigns = true },
+				handler = {
+					gitsigns = true,
+					color = colors.bg_highlight,
+				},
+				marks = {
+					Search = { color = colors.orange },
+					Error = { color = colors.error },
+					Warn = { color = colors.warning },
+					Info = { color = colors.info },
+					Hint = { color = colors.hint },
+					Misc = { color = colors.purple },
+				},
 			})
 		end,
 	},
