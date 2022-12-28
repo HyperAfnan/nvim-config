9d7dd94f1e6afc3cab15606ca9fbe9f487fcf01d
Mohd. Afnan <afnanmalik2712@gmail.com>
2022-12-28 22:26:10 +0530

    feat(ui): Add config for nvim-notify

--- lua/afnan/notify.lua
+++ lua/afnan/notify.lua
@@ -1,4 +1,3 @@
--- highlights
 local borders = {
 	"NotifyINFOBorder",
 	"NotifyERRORBorder",
@@ -14,18 +13,18 @@ vim.api.nvim_set_hl(0, "NotifyFg", { fg = "#a9b1d6", bg = "#1f2335" })
 vim.api.nvim_set_hl(0, "NotifyIcon", { fg = "#7dcfff", bg = "#1f2335" })
 vim.api.nvim_set_hl(0, "NotifyTitle", { fg = "#7dcfff", bg = "#1f2335" })
 
---for _, borderGroups in ipairs(borders) do
---	vim.highlight.link(borderGroups, "NotifyBg")
---end
---for _, iconGroups in ipairs(icons) do
---	vim.highlight.link(iconGroups, "NotifyIcon")
---end
---for _, titleGroups in ipairs(titles) do
---	vim.highlight.link(titleGroups, "NotifyTitle")
---end
---for _, bodyGroups in ipairs(bodies) do
---	vim.highlight.link(bodyGroups, "NotifyFg")
---end
+for _, borderGroups in ipairs(borders) do
+	vim.api.nvim_set_hl(0, borderGroups, { link = "NotifyBg" })
+end
+for _, iconGroups in ipairs(icons) do
+	vim.api.nvim_set_hl(0, iconGroups, { link = "NotifyIcon" })
+end
+for _, titleGroups in ipairs(titles) do
+	vim.api.nvim_set_hl(0, titleGroups, { link = "NotifyTitle" })
+end
+for _, bodyGroups in ipairs(bodies) do
+	vim.api.nvim_set_hl(0, bodyGroups, { link = "NotifyFg" })
+end
 
 local notify = require("notify")
 local default = {
